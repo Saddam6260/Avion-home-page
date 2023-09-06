d4195b0e894e02604e0fcd7a34b49e9ea18f1ada
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-09-06 14:11:18 +0600

    added footer without responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,18 @@ module.exports = {
     extend: {
       colors: {
         orange: "#F54C1E",
+        bgblack: "#031333",
       },
       fontFamily: {
         lato: ["lato"],
         inter: ["Inter"],
         poppins: ["Poppins"],
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
       },
     },
   },
